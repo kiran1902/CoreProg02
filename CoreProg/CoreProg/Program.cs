a90e623d3ed9da44c08e071a01aef62f2301d4c8
kiran1902 <kiru2kumar@gmail.com>
2022-03-10 21:57:59 +0530

    Coin Flip

---
+++ CoreProg/CoreProg/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Flip the Coin");
+const int ishead = 1;
+const int istail = 2;
+
+
+Random rand = new Random();
+int Tail = 0,
+    Head = 0,
+    Result = 0;
+
+int isCoinFlip = rand.Next(2);
+Console.WriteLine("isCoinFlip : " + isCoinFlip);
+
+if (isCoinFlip == 1)
+{
+    Console.WriteLine("It is Head");
+}
+else
+{
+    Console.WriteLine("It is Tail");
+}
