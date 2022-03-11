ac701ff740655d84b5cdb55032ce6b4a5fa24b5d
kiran1902 <kiru2kumar@gmail.com>
2022-03-11 20:22:42 +0530

    Power of Two

--- CoreProg/CoreProg/Program.cs
+++ CoreProg/CoreProg/Program.cs
@@ -1,43 +1,4 @@
-﻿/*-------------------------------------------------COIN FLIP-----------------------------------------------------------------------
-Console.WriteLine("Flip the Coin");
-const int ishead = 1;
-const int istail = 2;
-
-
-Random rand = new Random();
-int Tail = 0,
-    Head = 0,
-    Result = 0;
-
-int isCoinFlip = rand.Next(2);
-Console.WriteLine("isCoinFlip : " + isCoinFlip);
-
-if (isCoinFlip == 1)
-{
-    Console.WriteLine("It is Head");
-}
-else
-{
-    Console.WriteLine("It is Tail");
-}
-
-------------------------------------------------------~~~~~~~~--------------------------------------------------------------------*/
-
-/*--------------------------------------------------------LEAP YEAR------------------------------------------------------------------
-
-Console.WriteLine("Enter The Year : ");
-int year = int.Parse(Console.ReadLine());
-if ((year %4 == 0) && (year %100 != 0) || (year % 400 == 0))
-{
-    Console.WriteLine("It is leap year");
-}
-else
-{
-    Console.WriteLine("It is not leap year");
-}
-
-------------------------------------------------------~~~~~~~~--------------------------------------------------------------------*/
-
+﻿/*-----------------------------------------------------------PowerOfTWo-----------------------------------------------------------*/
 Console.WriteLine("Enter the Power of 2 :");
 int n = int.Parse(Console.ReadLine());
 int i = 0;
@@ -48,6 +9,7 @@ while (i <= n)
     powerofTwo = 2 * powerofTwo;
     i = i + 1;
 }
+
 
 
 
