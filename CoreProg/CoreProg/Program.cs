946eb392ca27680a3293402e632ad03ef35711db
kiran1902 <kiru2kumar@gmail.com>
2022-03-11 20:58:06 +0530

    Prime Factors

--- CoreProg/CoreProg/Program.cs
+++ CoreProg/CoreProg/Program.cs
@@ -23,7 +23,7 @@ else
 
 ------------------------------------------------------~~~~~~~~--------------------------------------------------------------------*/
 
-/*--------------------------------------------------------LEAP YEAR------------------------------------------------------------------*/
+/*--------------------------------------------------------LEAP YEAR------------------------------------------------------------------
 
 Console.WriteLine("Enter The Year : ");
 int year = int.Parse(Console.ReadLine());
@@ -35,3 +35,42 @@ else
 {
     Console.WriteLine("It is not leap year");
 }
+-----------------------------------------------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------PowerOfTWo-----------------------------------------------------------
+Console.WriteLine("Enter the Power of 2 :");
+int n = int.Parse(Console.ReadLine());
+int i = 0;
+int powerofTwo = 1;
+while (i <= n)
+{
+    Console.WriteLine(i + " " + powerofTwo);
+    powerofTwo = 2 * powerofTwo;
+    i = i + 1;
+}
+-----------------------------------------------------------------------------------------------------------------------------------*/
+
+/*---------------------------------------------Harmonic Number----------------------------------------------------------------------
+int i, n;
+double s = 0.0;
+Console.Write("Calculate the harmonic series and their sum:\n");
+Console.Write("Input the number of terms : ");
+n = Convert.ToInt32(Console.ReadLine());
+Console.Write("\n\n");
+for (i = 1; i <= n; i++)
+{
+    Console.Write("1/{0} + ", i);
+    s += 1 / (float)i;
+}
+Console.Write("Sum of Series upto {0} terms : {1} ", n, s);
+-----------------------------------------------------------------------------------------------------------------------------------*/
+int a, b;
+Console.WriteLine("Please enter your integer: ");
+a = int.Parse(Console.ReadLine());
+for (b = 1; b <= a; b++)
+{
+    if (a % b == 0)
+    {
+        Console.WriteLine(b + " is a factor of " + a);
+    }
+}
+Console.ReadLine();
