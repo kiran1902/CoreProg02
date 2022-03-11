16c0b8521ace3d57a5f48f39af3413c2c1089b72
kiran1902 <kiru2kumar@gmail.com>
2022-03-11 14:25:27 +0530

    Leap Year

--- CoreProg/CoreProg/Program.cs
+++ CoreProg/CoreProg/Program.cs
@@ -1,28 +1,4 @@
-﻿/*-------------------------------------------------COIN FLIP-----------------------------------------------------------------------
-Console.WriteLine("Flip the Coin");
-const int ishead = 1;
-const int istail = 2;
-
-
-Random rand = new Random();
-int Tail = 0,
-    Head = 0,
-    Result = 0;
-
-int isCoinFlip = rand.Next(2);
-Console.WriteLine("isCoinFlip : " + isCoinFlip);
-
-if (isCoinFlip == 1)
-{
-    Console.WriteLine("It is Head");
-}
-else
-{
-    Console.WriteLine("It is Tail");
-}
-
-------------------------------------------------------~~~~~~~~--------------------------------------------------------------------*/
-
+﻿
 /*--------------------------------------------------------LEAP YEAR------------------------------------------------------------------*/
 
 Console.WriteLine("Enter The Year : ");
